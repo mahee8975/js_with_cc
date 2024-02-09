894842e819e1b367496c4aa6ba54adf30b6f4ece
mahee8975 <147308496+mahee8975@users.noreply.github.com>
2024-02-09 09:02:23 +0000

    hitesh

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,9 @@
+const accountId = 145554
+let accountemail = "mahesh@gamil"
+var accountPass = "1234"
+accountCity = "jaipur"
+
+// accountId = 12 
+
+console.log(accountId);
+console.table([accountId, accountPass, accountemail])
